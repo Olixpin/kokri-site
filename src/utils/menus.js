ee91e91db1c3c3c2e2d25d6af348a8395dd17aea
Olixpin <noibiabdulrasaq@gmail.com>
2023-04-10 00:23:03 +0100

    Add Contact Page

--- src/utils/menus.js
+++ src/utils/menus.js
@@ -1,33 +1,33 @@
 const menus = [
-    {
-        id: 1,
-        name: 'Work',
-        path: '/work',
-    },
+  {
+    id: 1,
+    name: "Work",
+    path: "/work",
+  },
 
-    {
-        id: 2,
-        name: 'About',
-        path: '/about',
-    },
+  {
+    id: 2,
+    name: "About",
+    path: "/about",
+  },
 
-    {
-        id: 3,
-        name: 'Live',
-        path: '/live',
-    },
+  // {
+  //     id: 3,
+  //     name: 'Live',
+  //     path: '/live',
+  // },
 
-    {
-        id: 4,
-        name: 'Careers',
-        path: '/careers',
-    },
+  {
+    id: 4,
+    name: "Careers",
+    path: "/careers",
+  },
 
-    {
-        id: 5,
-        name: 'Contact',
-        path: '/contact',
-    }
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact",
+  },
 ]
 
 export default menus
